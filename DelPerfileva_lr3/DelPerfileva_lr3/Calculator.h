d607cdaa0faca49fb4a33d86c60b2a49d26c4037
DELALEX <tania377033122@gmail.com>
2021-12-23 20:58:53 +0300

    v1.5
    Added some logic
    Some bug fixes

--- DelPerfileva_lr3/DelPerfileva_lr3/Calculator.h
+++ DelPerfileva_lr3/DelPerfileva_lr3/Calculator.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "prototypes.h"
 
 namespace CppCLRWinformsProjekt {
 
@@ -35,15 +36,20 @@ namespace CppCLRWinformsProjekt {
 			}
 		}
 	private: System::Windows::Forms::Button^ button_calculate;
-	private: System::Windows::Forms::TextBox^ textBox_last_counter;
 
-	private: System::Windows::Forms::TextBox^ textBox_current_counter;
-	private: System::Windows::Forms::TextBox^ textBox_price;
+
+
+
 
 
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label_result;
+	private: System::Windows::Forms::NumericUpDown^ value_current;
+	private: System::Windows::Forms::NumericUpDown^ value_previous;
+	private: System::Windows::Forms::NumericUpDown^ value_price;
+
 	protected:
 
 	protected:
@@ -66,44 +72,27 @@ namespace CppCLRWinformsProjekt {
 		void InitializeComponent(void)
 		{
 			this->button_calculate = (gcnew System::Windows::Forms::Button());
-			this->textBox_last_counter = (gcnew System::Windows::Forms::TextBox());
-			this->textBox_current_counter = (gcnew System::Windows::Forms::TextBox());
-			this->textBox_price = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label_result = (gcnew System::Windows::Forms::Label());
+			this->value_current = (gcnew System::Windows::Forms::NumericUpDown());
+			this->value_previous = (gcnew System::Windows::Forms::NumericUpDown());
+			this->value_price = (gcnew System::Windows::Forms::NumericUpDown());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->value_current))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->value_previous))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->value_price))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button_calculate
 			// 
-			this->button_calculate->Location = System::Drawing::Point(15, 137);
+			this->button_calculate->Location = System::Drawing::Point(15, 164);
 			this->button_calculate->Name = L"button_calculate";
 			this->button_calculate->Size = System::Drawing::Size(129, 49);
 			this->button_calculate->TabIndex = 0;
 			this->button_calculate->Text = L"Рассчитать";
 			this->button_calculate->UseVisualStyleBackColor = true;
 			this->button_calculate->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
-			// 
-			// textBox_last_counter
-			// 
-			this->textBox_last_counter->Location = System::Drawing::Point(199, 30);
-			this->textBox_last_counter->Name = L"textBox_last_counter";
-			this->textBox_last_counter->Size = System::Drawing::Size(233, 20);
-			this->textBox_last_counter->TabIndex = 1;
-			// 
-			// textBox_current_counter
-			// 
-			this->textBox_current_counter->Location = System::Drawing::Point(199, 62);
-			this->textBox_current_counter->Name = L"textBox_current_counter";
-			this->textBox_current_counter->Size = System::Drawing::Size(233, 20);
-			this->textBox_current_counter->TabIndex = 2;
-			// 
-			// textBox_price
-			// 
-			this->textBox_price->Location = System::Drawing::Point(199, 98);
-			this->textBox_price->Name = L"textBox_price";
-			this->textBox_price->Size = System::Drawing::Size(233, 20);
-			this->textBox_price->TabIndex = 3;
 			// 
 			// label1
 			// 
@@ -135,23 +124,57 @@ namespace CppCLRWinformsProjekt {
 			this->label3->TabIndex = 6;
 			this->label3->Text = L"Цена в рублях за кВт:";
 			// 
+			// label_result
+			// 
+			this->label_result->AutoSize = true;
+			this->label_result->Location = System::Drawing::Point(12, 137);
+			this->label_result->Name = L"label_result";
+			this->label_result->Size = System::Drawing::Size(34, 13);
+			this->label_result->TabIndex = 7;
+			this->label_result->Text = L"Итог:";
+			// 
+			// value_current
+			// 
+			this->value_current->Location = System::Drawing::Point(199, 63);
+			this->value_current->Name = L"value_current";
+			this->value_current->Size = System::Drawing::Size(233, 20);
+			this->value_current->TabIndex = 8;
+			// 
+			// value_previous
+			// 
+			this->value_previous->Location = System::Drawing::Point(199, 31);
+			this->value_previous->Name = L"value_previous";
+			this->value_previous->Size = System::Drawing::Size(233, 20);
+			this->value_previous->TabIndex = 9;
+			// 
+			// value_price
+			// 
+			this->value_price->Location = System::Drawing::Point(199, 101);
+			this->value_price->Name = L"value_price";
+			this->value_price->Size = System::Drawing::Size(233, 20);
+			this->value_price->TabIndex = 10;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->ClientSize = System::Drawing::Size(595, 380);
+			this->Controls->Add(this->value_price);
+			this->Controls->Add(this->value_previous);
+			this->Controls->Add(this->value_current);
+			this->Controls->Add(this->label_result);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox_price);
-			this->Controls->Add(this->textBox_current_counter);
-			this->Controls->Add(this->textBox_last_counter);
 			this->Controls->Add(this->button_calculate);
 			this->ForeColor = System::Drawing::SystemColors::ControlText;
 			this->Name = L"Form1";
 			this->Text = L"Calculator";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->value_current))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->value_previous))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->value_price))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -160,22 +183,36 @@ namespace CppCLRWinformsProjekt {
 	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) 
 	{
 		// onFormLoaded
-
 	}
+
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 		//onCalculateButtonClick
+		
+		//проверка длины полей, предотвращение вылета программы
+		int previous_value_lenght = value_previous->Text->Length;
+		int current_value_lenght = value_current->Text->Length;
+		int price_lenght = value_price->Text->Length;
 
+		//получение значений из текстовых полей
+		if (previous_value_lenght > 0)
+			int previous_value = Convert::ToInt32(value_previous->Text);
+		if (current_value_lenght > 0)
+			int current_value = Convert::ToInt32(value_current->Text);
+		if (price_lenght > 0)
+			int price = Convert::ToInt32(value_price->Text);
+	
+		//вывод результата
+	
+	
 	}
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 		// NO NEED THIS
-
 	}
-private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e)
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e)
 	{
 		//NO NEED THIS EVENT
-
 	}
 };
 }
---
+++ DelPerfileva_lr3/DelPerfileva_lr3/main.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "libraries.h"
+#include "Calculator.h"
+
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew CppCLRWinformsProjekt::Form1()); 
+	return 0;
+}
