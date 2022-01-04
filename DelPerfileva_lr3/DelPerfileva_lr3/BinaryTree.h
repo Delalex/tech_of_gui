d6972976be24a48441f5d8c8a94af35ffe7071e4
DELALEX <tania377033122@gmail.com>
2022-01-04 16:58:26 +0300

    v5.0

--- DelPerfileva_lr3/DelPerfileva_lr3/BinaryTree.h
+++ DelPerfileva_lr3/DelPerfileva_lr3/BinaryTree.h
@@ -34,6 +34,14 @@ namespace CppCLRWinformsProjekt {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+	protected:
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ buildTree;
+	private: System::Windows::Forms::TreeView^ tree;
+
 
 	private:
 		/// <summary>
@@ -48,17 +56,112 @@ namespace CppCLRWinformsProjekt {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->buildTree = (gcnew System::Windows::Forms::Button());
+			this->tree = (gcnew System::Windows::Forms::TreeView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(155)),
+				static_cast<System::Int32>(static_cast<System::Byte>(155)));
+			this->numericUpDown1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->numericUpDown1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->numericUpDown1->Location = System::Drawing::Point(176, 17);
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(294, 45);
+			this->numericUpDown1->TabIndex = 0;
+			// 
+			// numericUpDown2
+			// 
+			this->numericUpDown2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(155)),
+				static_cast<System::Int32>(static_cast<System::Byte>(155)));
+			this->numericUpDown2->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->numericUpDown2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->numericUpDown2->Location = System::Drawing::Point(176, 68);
+			this->numericUpDown2->Name = L"numericUpDown2";
+			this->numericUpDown2->Size = System::Drawing::Size(294, 45);
+			this->numericUpDown2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(155)),
+				static_cast<System::Int32>(static_cast<System::Byte>(155)));
+			this->label1->Location = System::Drawing::Point(7, 20);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(140, 39);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Число P:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(155)),
+				static_cast<System::Int32>(static_cast<System::Byte>(155)));
+			this->label2->Location = System::Drawing::Point(7, 68);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(162, 39);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Уровни N:";
+			// 
+			// buildTree
+			// 
+			this->buildTree->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(155)),
+				static_cast<System::Int32>(static_cast<System::Byte>(155)));
+			this->buildTree->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->buildTree->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(7)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->buildTree->Location = System::Drawing::Point(14, 127);
+			this->buildTree->Name = L"buildTree";
+			this->buildTree->Size = System::Drawing::Size(456, 37);
+			this->buildTree->TabIndex = 5;
+			this->buildTree->Text = L"Построить";
+			this->buildTree->UseVisualStyleBackColor = false;
+			this->buildTree->Click += gcnew System::EventHandler(this, &BinaryTree::buildTree_Click);
+			// 
+			// tree
+			// 
+			this->tree->Location = System::Drawing::Point(14, 187);
+			this->tree->Name = L"tree";
+			this->tree->Size = System::Drawing::Size(462, 347);
+			this->tree->TabIndex = 6;
 			// 
 			// BinaryTree
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->ClientSize = System::Drawing::Size(1298, 546);
+			this->Controls->Add(this->tree);
+			this->Controls->Add(this->buildTree);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->numericUpDown2);
+			this->Controls->Add(this->numericUpDown1);
 			this->Name = L"BinaryTree";
 			this->Text = L"BinaryTree";
 			this->Load += gcnew System::EventHandler(this, &BinaryTree::BinaryTree_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -66,5 +169,15 @@ namespace CppCLRWinformsProjekt {
 	{
 		this->BackgroundImage = Bitmap::FromFile(Application::StartupPath + "\\rainbow.png"); // Загрузка фона
 	}
-	};
+	private: System::Void buildTree_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		//Строительство бинарного дерева
+		TreeNode^ node = gcnew TreeNode("Уровень 1"); // добавление узла
+		tree->Nodes->Add(node);
+		TreeNode^ subnode = gcnew TreeNode("Уровень 2"); // добавление подузла
+		node->Nodes->Add(subnode);
+		TreeNode^ ultranode = gcnew TreeNode("Уровень 3"); // добавление подузла
+		subnode->Nodes->Add(ultranode);
+	}
+};
 }
